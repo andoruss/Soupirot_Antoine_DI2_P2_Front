8c2555370c25b2e7662492fb1423cbdbcc70d080
Souruss <soupirot.dev@gmail.com>
2024-03-06 16:30:09 +0100

    Add event creation form component

--- EvaluationFrontEnd/src/app/app.component.ts
+++ EvaluationFrontEnd/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { EventListComponent } from './component/event-list/event-list.component';
+import { EventCreationComponent } from './component/event-creation/event-creation.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, EventListComponent],
+  imports: [CommonModule, RouterOutlet, EventListComponent, EventCreationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ EvaluationFrontEnd/src/app/component/event-creation/event-creation.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { EventService } from '../../core/services/event/event.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Events } from '../../shared/model/event';
+
+@Component({
+  selector: 'app-event-creation',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './event-creation.component.html',
+  styleUrl: './event-creation.component.scss'
+})
+export class EventCreationComponent {
+  showpopup = false;
+  model = {
+    title: '',
+    date: new Date(),
+    location: '',
+    description: ''
+  }
+
+  constructor(private eventService: EventService) { }
+
+  createEvent(){
+    const event: Events = {
+      ...this.model,
+      id: ''
+    }
+    this.eventService.addEvent(event);
+    this.showpopup = false;
+  }
+}
